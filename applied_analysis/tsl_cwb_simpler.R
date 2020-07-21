0f7f50665f436ab7573e75eaddd26932129c91ea
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-07-21 16:40:00 -0500

    cwb simpler

--- applied_analysis/tsl_cwb_simpler.R
+++ applied_analysis/tsl_cwb_simpler.R
@@ -15,13 +15,19 @@ tsl_dat_small <- tsl_dat %>%
   group_by(studyid) %>%
   mutate(k_j = n_distinct(esid),
          sigma_sq_j = mean(v),
-         w_j = 1/(k_j * sigma_sq_j))
+         w_tilde_j = 1/(k_j * sigma_sq_j)) %>%
+  ungroup()
 
 
-mod_prelim <- lm(delta ~ dv + g2age, data = tsl_dat_small, weights = w_j)
-qe <- deviance(mod_prelim)
+mod_prelim <- lm(delta ~ dv + g2age, data = tsl_dat_small, weights = w_tilde_j)
 
 
+length(residuals(mod_prelim))
+
+tsl_dat_small <- tsl_dat_small %>%
+  mutate(res = residuals(mod_prelim))
+
+QE <- with(tsl_dat_small, sum(w_tilde_j * res^2))
 
 # calculate v -------------------------------------------------------------
 
@@ -35,13 +41,29 @@ Xj <- full_x %>%
 
 X_j_mat <- group_split(Xj)
 
-w_dat <- tsl_dat_small %>%
-  select(studyid, w_j) %>%
-  distinct(.)
+w_tilde_dat <- tsl_dat_small %>%
+  select(studyid, w_tilde_j) %>%
+  distinct(.) %>%
   group_by(studyid)
 
-wj <- group_split(w_dat)
+w_tilde_j <- group_split(w_tilde_dat)
 
+
+num_es <- tsl_dat_small %>%
+  select(studyid, k_j) %>%
+  distinct(.) %>%
+  select(k_j)
+
+create_identity <- function(k){
+  
+  diag(k)
+  
+}
+
+
+k <- num_es %>% pull(k_j)
+
+I_j <- map(k, create_identity)
 
 change_to_mat <- function(dat){
   
@@ -55,26 +77,61 @@ change_to_vec <- function(dat){
   
   dat %>%
     select(-studyid) %>%
-    pull(w_j)
+    pull(w_tilde_j)
 }
 
 
 X_j <- map(X_j_mat, change_to_mat)
-w_j <- map(wj, change_to_vec)
 
-calc_v <- function(x, w){
+w_tilde_j <- map(w_tilde_j, change_to_vec)
+
+create_big_W <- function(w, id){
   
-  x_m <- t(x) %*% x
-
-  w * x_m
+  w * id
   
-} 
+}
+
+W_tilde_j <- pmap(list(w_tilde_j, I_j), create_big_W)
 
 
-V_all <- pmap(list(X_j, w_j), calc_v)
+create_m_tilde <- function(X, W){
+  
+  t(X) %*% W %*% X
+  
+}
 
-V <- solve(V_all %>% reduce(`+`)) 
+M_tilde_all <- pmap(list(X_j, W_tilde_j), create_m_tilde)
 
-# wts for corr effects model
-# wij = 1/ (kj * (sigma sq j + tau sq))
+M_tilde <- M_tilde_all %>% 
+  reduce(`+`) %>%
+  solve()
+
+
+calculate_p <- function(X){
+  
+  X %>%
+    as_tibble() %>%
+    summarise_all(sum) %>%
+    as.matrix()
+  
+}
+
+
+p_j <- map(X_j, calculate_p)
+
+
+m <- 75
+rho <- .8
+
+
+trace_product <- function(A, B) {
+  
+  a_vec <- as.vector(t(A))
+  b_vec <- as.vector(B)
+  sum(a_vec * b_vec)
+  
+}
+
+
+
 
