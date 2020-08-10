87b10329e50f0a95b241123307505cc92f1d9d61
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-08-10 11:38:59 -0500

    estimation run through

---
+++ estimation/1_estimation_run_through.R
@@ -0,0 +1,157 @@
+library(tidyverse)
+library(robumeta)
+library(clubSandwich)
+
+# files -------------------------------------------------------------------
+load("data/meta_data_practice.RData")
+
+# combinations ------------------------------------------------------------
+
+covs <- c("X1", "X2", "X3", "X4", "X5")
+
+# terms combinations
+comb_terms <- function(m, terms = covs) combn(length(terms), m, simplify = FALSE)
+
+indices <- unlist(map(seq_along(1:5), comb_terms), recursive = FALSE)
+equations <- map_chr(indices, function(x) paste(terms[x], collapse = "+"))
+
+full_mod_indices <- 1:5
+
+
+# null model 
+null_indices <- map(indices, function(x) full_mod_indices[-x])
+null_terms <- map(null_indices, function(x) terms[x])
+null_model <- map_chr(null_terms, function(x) paste(x, collapse = "+"))
+
+
+# all the null model to fit
+to_test <- tibble(equ = equations,
+                  type = c(rep("single", 5), rep("mch", 26)),
+                  indices = indices) %>%
+  mutate(null_indices = null_indices,
+         null_terms = null_terms, 
+         null_model = null_model, 
+         null_model = if_else(null_model == "", "1", null_model))
+
+
+# full model --------------------------------------------------------------
+
+dat <- meta_data
+full_formula <- "smd ~ X1 + X2 + X3 + X4 + X5"
+
+
+full_mod <- robu(as.formula(full_formula), 
+                 studynum = study, 
+                 var.eff.size = var_smd,
+                 small = FALSE,
+                 data = dat)
+
+full_mod
+
+# function to run wald test -----------------------------------------------
+
+
+get_wald <- function(model, constraints, cov_mat, test){
+         
+  Wald_test(model, 
+            constraints = constrain_zero(constraints), 
+            vcov = cov_mat,
+            test = test)
+}
+
+
+# run wald ----------------------------------------------------------------
+
+cov_mat_cr1 <- vcovCR(full_mod, type = "CR1")
+cov_mat_cr2 <- vcovCR(full_mod, type = "CR2")
+
+full_mod <- list(full_mod)
+cov_mat_cr1 <- list(cov_mat_cr1)
+cov_mat_cr2 <- list(cov_mat_cr2)
+naive <- list("Naive-F")
+htz <- list("HTZ")
+
+
+
+system.time(naive_test <- pmap_dfr(list(full_mod, to_test$indices, cov_mat_cr1, "Naive-F"), get_wald))
+system.time(htz_test <- pmap_dfr(list(full_mod, to_test$indices, cov_mat_cr2, "HTZ"), get_wald))
+
+naive_res <- bind_cols(to_test %>% select(equ, type), naive_test)
+htz_res <- bind_cols(to_test %>% select(equ, type), htz_test)
+
+# CWB ---------------------------------------------------------------------
+
+# fit null mods
+
+fit_mod <- function(equation, dat = meta_data){
+  
+  robu(as.formula(equation), 
+       studynum = study, 
+       var.eff.size = var_smd,
+       small = FALSE,
+       data = dat)
+
+}
+
+params <- tibble(equation = paste("smd ~ ", to_test$null_model)) 
+  
+system.time(null_mods <- map(params$equation, fit_mod))
+
+
+# extract residuals -------------------------------------------------------
+
+
+change_to_mat <- function(res){
+  
+  as.matrix(res)
+  
+}
+
+mult_mat <- function(x, y){
+  
+  x %*% y
+  
+}
+
+
+# extract the residuals
+
+extract_res <- function(mod, dat = meta_data){
+
+  res <- clubSandwich:::residuals_CS.robu(mod)
+  study <- dat$study
+
+  split_res <- split(res, study)
+
+  e_tilde_j <- map(split_res, change_to_mat)
+
+  return(list(e_tilde_j))
+
+}
+
+system.time(null_res <- map(null_mods, extract_res))
+
+
+
+# extract null cr models --------------------------------------------------
+
+# this is not running
+
+extract_B <- function(mod){
+  
+  attr(vcovCR(mod, type = "CR2"), "adjustments")
+  
+}
+
+extract_B(null_mods[[31]])  # works till 30
+# 31 doesn't have a vcov adj matrix 
+# because the model only has an intercept
+
+
+system.time(null_B <- map(null_mods[1:30], extract_B))
+
+
+
+# multiply residuals by adj matrices --------------------------------------
+
+
---
+++ estimation/scratch/null_mod.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+
+
+load("data/meta_data_practice.RData")
+
+vars <- names(meta_data %>% select(starts_with("X")))[2:6]
+
+dat <- tibble(cov = c("X1", "X2", "X3", "X4", "X5"),
+       index = c(1:5))
+
+# terms combinations
+terms <- dat$cov
+comb_terms <- function(m, terms = dat$cov) combn(length(terms), m, simplify = FALSE)
+
+indices <- unlist(map(seq_along(1:5), comb_terms), recursive = FALSE)
+equations <- map_chr(indices, function(x) paste(terms[x], collapse = "+"))
+
+full_mod_indices <- c(1, 2, 3, 4, 5)
+
+
+# null model 
+null_indices <- map(indices, function(x) full_mod_indices[-x])
+null_terms <- map(null_indices, function(x) terms[x])
+null_model <- map_chr(null_terms, function(x) paste(x, collapse = "+"))
+
+
+# all the null model to fit
+to_test <- tibble(equ = equations,
+                  type = c(rep("single", 5), rep("mch", 26)),
+                  indices = indices) %>%
+  mutate(null_indices = null_indices,
+         null_terms = null_terms, 
+         null_model = null_model)
+
+
+save(to_test, file = "data/to_test.RData")
+
