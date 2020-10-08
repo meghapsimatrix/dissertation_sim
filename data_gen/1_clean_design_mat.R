f94e40413dacca245885f9d2e2e2b2feadad4957
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-10-08 13:22:42 -0500

    design mat

--- data_gen/1_clean_design_mat.R
+++ data_gen/1_clean_design_mat.R
@@ -9,6 +9,9 @@ design_mat <- read_csv("data/design_matrix.csv") %>%
          X = 1) %>%
   select(X, everything())
 
+
+sample(c(rep("A", 9), rep("B", 9), rep("C", 2)))
+
 design_mat$X6 <- rep(sample(c("A", "B", "C"), size = 20, replace = TRUE), each = 10)
 
 design_mat <- dummy_cols(design_mat, select_columns = "X6") %>%
@@ -17,3 +20,6 @@ design_mat <- dummy_cols(design_mat, select_columns = "X6") %>%
 glimpse(design_mat)
 
 save(design_mat, file = "data/design_mat.Rdata")
+
+
+
