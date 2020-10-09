02c913fddd0fa2aa5f3d09f336229979208e8a75
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-10-09 12:16:03 -0500

    sim run through

--- sim_tacc/4_run_sim_study_1.R
+++ sim_tacc/4_run_sim_study_1.R
@@ -26,38 +26,38 @@ source("3_performance_criteria.R")
 run_sim <- function(iterations, m, tau, rho, beta_type, test_dat = to_test, seed = NULL) {
   
 
-      # non zero betas only for power -------------------------------------------
+  # non zero betas only for power -------------------------------------------
       
-      if (beta_type %in% c("B1", "B5")){
+  if (beta_type %in% c("B1", "B5")){
         
-        test_dat <- test_dat %>%
-          filter(str_detect(cov_test, "X1"))
+      test_dat <- test_dat %>%
+        filter(str_detect(cov_test, "X1"))
         
-      } else if(beta_type %in% c("C1", "C5")){
+  } else if(beta_type %in% c("C1", "C5")){
         
-        test_dat <- test_dat %>%
-          filter(str_detect(cov_test, "X2"))
+      test_dat <- test_dat %>%
+        filter(str_detect(cov_test, "X2"))
       
-      } else if(beta_type %in% c("D1", "D5")){
+  } else if(beta_type %in% c("D1", "D5")){
         
-        test_dat <- test_dat %>%
-          filter(str_detect(cov_test, "X3"))
+    test_dat <- test_dat %>%
+      filter(str_detect(cov_test, "X3"))
         
-      } else if(beta_type %in% c("E1", "E5")){
+  } else if(beta_type %in% c("E1", "E5")){
         
-        test_dat <- test_dat %>%
-          filter(str_detect(cov_test, "X4"))
+    test_dat <- test_dat %>%
+      filter(str_detect(cov_test, "X4"))
         
-      } else if(beta_type %in% c("F1", "F5")){
+  } else if(beta_type %in% c("F1", "F5")){
         
-        test_dat <- test_dat %>%
-          filter(str_detect(cov_test, "X5"))
+    test_dat <- test_dat %>%
+      filter(str_detect(cov_test, "X5"))
         
-      } else if(beta_type == "A" ){
+  } else if(beta_type == "A" ){
         
-        test_dat <- test_dat 
+    test_dat <- test_dat 
         
-      } 
+  } 
       
     
   if (!is.null(seed)) set.seed(seed)
