755c3895acab3efb4a5a1d9aa520685d1d7fd516
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2020-11-27 18:11:48 -0600

    test

--- software/test.R
+++ software/test.R
@@ -1,5 +1,23 @@
 load("/Users/meghajoshi/Box Sync/Dissertation_Joshi/simulation/data/meta_data_practice.RData")
 library(wildmeta)
+library(robumeta)
+library(clubSandwich)
+
+system.time(res <- cwb(dat = meta_data,
+                       smd = g,
+                       var = var_g,
+                       cluster = study,
+                       covs_full_form <- "X1 + X2 + X3 + X4 + X5", 
+                       indices = 2:6))
+
+system.time(res_adj <- cwb(dat = meta_data,
+                       smd = g,
+                       var = var_g,
+                       cluster = study,
+                       covs_full_form <- "X1 + X2 + X3 + X4 + X5", 
+                       indices = 2:6,
+                       adjust = TRUE))
+
 
 full <- robu(g ~ X1 + X2 + X3 + X4 + X5,
              studynum = study,
@@ -7,20 +25,16 @@ full <- robu(g ~ X1 + X2 + X3 + X4 + X5,
              small = FALSE,
              data = meta_data)
 
-null <- robu(g ~ 1,
-             studynum = study,
-             var.eff.size = var_g,
-             small = FALSE,
-             data = meta_data)
-
-
-system.time(res <- cwb(data = meta_data,
-                       smd = g,
-                       var = var_g,
-                       cluster = study,
-                       full_model = full,
-                       null_model = null, 
-                       indices = 2:6))
-
 Wald_test(full, constraints = constrain_zero(2:6), vcov = "CR2")
+
+
+cwb(dat = SATcoaching,
+    smd = d,
+    var = V,
+    cluster = study,
+    covs_full_form = "study_type",
+    test_vars = "study_type",
+    indices = 2:3,
+    R = 99)
+
 
